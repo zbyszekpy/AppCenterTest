9137b9d22db78071139e35991440776cc985b452
Zbyszek <zbyszekpy@gmail.com>
2018-10-08 20:16:57 +0200

    initial version

---
+++ AppCenterTestApp/AppCenterTestApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppCenterTestApp.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppCenterTestApp
+{
+  public partial class App : Application
+  {
+
+    public App()
+    {
+      InitializeComponent();
+
+
+      MainPage = new MainPage();
+    }
+
+    protected override void OnStart()
+    {
+      // Handle when your app starts
+    }
+
+    protected override void OnSleep()
+    {
+      // Handle when your app sleeps
+    }
+
+    protected override void OnResume()
+    {
+      // Handle when your app resumes
+    }
+  }
+}
