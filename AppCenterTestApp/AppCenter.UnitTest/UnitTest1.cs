43876056e82342fa22422ff204ea4493681c66f6
Zbyszek <zbyszekpy@gmail.com>
2018-10-08 20:41:39 +0200

    unit and UI tests added

---
+++ AppCenterTestApp/AppCenter.UnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace AppCenter.UnitTest
+{
+  using System.Linq;
+
+  using AppCenterTestApp.Models;
+  using AppCenterTestApp.Services;
+
+  using Xunit;
+
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+      var store = new MockDataStore();
+
+      Item item = store.GetItemsAsync().Result.First();
+      Assert.NotNull(item);
+      Assert.False(true);
+    }
+  }
+}
